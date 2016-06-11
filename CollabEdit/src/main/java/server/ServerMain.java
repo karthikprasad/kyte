ecf430a81a9c3171c1f94f93a1fea6492744761f
tanoojp <tanoojp@gmail.com>
2016-06-11 11:37:49 -0700

    ClientServer startup initialization

--- CollabEdit/src/main/java/server/ServerMain.java
+++ CollabEdit/src/main/java/server/ServerMain.java
@@ -3,19 +3,52 @@ package server;
 import java.io.File;
 
 import db.PPSDBConnector;
+import model.PPSCache;
 
 public class ServerMain {
 
 	PPSDBConnector dbConnector;
 	RequestHandler requestHandler;
 	MessageProcessor messageProcessor;
-
+	PPSCache cache;
 	
-	public void init()
+	private static ServerMain instance;
+	
+	private ServerMain(){}
+	
+	private void init()
 	{
 		dbConnector = new PPSDBConnector(new File(ServerConfig.ENV_HOME), ServerConfig.GRP_NAME, 
 				ServerConfig.NODE_NAME, ServerConfig.NODE_HOST_PORT, ServerConfig.HELPER_HOST_PORT);
 		messageProcessor = new MessageProcessor(dbConnector);
 		requestHandler = new RequestHandler(messageProcessor);
+		cache = new PPSCache();
+	}
+
+	public static ServerMain getInstance()
+	{
+		if(instance == null)
+			instance = new ServerMain();
+		instance.init();
+		return instance;
+	}
+	
+	public PPSDBConnector getDbConnector() {
+		return dbConnector;
+	}
+
+
+	public RequestHandler getRequestHandler() {
+		return requestHandler;
+	}
+
+
+	public MessageProcessor getMessageProcessor() {
+		return messageProcessor;
+	}
+
+
+	public PPSCache getCache() {
+		return cache;
 	}
 }
