cb95359358088009021614d0138c9c901c43b9d7
tanoojp <tanoojp@gmail.com>
2016-06-11 16:05:20 -0700

    Added cursor to getAllEntries from PPSDBConnector

--- CollabEdit/src/main/java/db/PPSDBConnector.java
+++ CollabEdit/src/main/java/db/PPSDBConnector.java
@@ -1,10 +1,13 @@
 package db;
 import java.io.File;
 import java.io.UnsupportedEncodingException;
+import java.util.TreeMap;
 
+import com.sleepycat.je.Cursor;
 import com.sleepycat.je.Database;
 import com.sleepycat.je.DatabaseConfig;
 import com.sleepycat.je.DatabaseEntry;
+import com.sleepycat.je.DatabaseException;
 import com.sleepycat.je.EnvironmentConfig;
 import com.sleepycat.je.LockMode;
 import com.sleepycat.je.OperationStatus;
@@ -80,6 +83,36 @@ public class PPSDBConnector {
           c = (char) retData[0];
       }
       return c;
+	}
+
+	public TreeMap<Double, Character> getAllEntries()
+	{
+		Cursor myCursor = db.openCursor(null, null);
+		TreeMap<Double, Character> map = new TreeMap<Double, Character>();
+        DatabaseEntry foundKey = new DatabaseEntry();
+        DatabaseEntry foundData = new DatabaseEntry();
+     
+        while (myCursor.getNext(foundKey, foundData, LockMode.DEFAULT) ==
+            OperationStatus.SUCCESS) {
+            String keyString;
+			try {
+				keyString = new String(foundKey.getData(), "UTF-8");
+				Double key = Double.parseDouble(keyString);
+				Character c = (char) foundData.getData()[0];
+				map.put(key, c);
+				
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+        }
+        try {
+            if (myCursor != null) {
+                myCursor.close();
+            }
+        } catch(DatabaseException dbe) {
+            System.err.println("Error closing cursor: " + dbe.toString());
+        }
+        return map;
 	}
 	
 	public void closeDB()
