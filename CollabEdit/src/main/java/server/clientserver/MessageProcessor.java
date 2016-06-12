81f63431f34eb2aea9a4b9e4a215e35c56c64583
tanoojp <tanoojp@gmail.com>
2016-06-11 21:29:09 -0700

    MessageProcessor handle write messages

--- CollabEdit/src/main/java/server/clientserver/MessageProcessor.java
+++ CollabEdit/src/main/java/server/clientserver/MessageProcessor.java
@@ -3,8 +3,10 @@ package server.clientserver;
 import db.PPSDBConnector;
 import model.Message;
 import model.MessageResponse;
+import model.PPSCache;
 import model.RegisterMessage;
 import model.RegisterMessageResponse;
+import model.WriteMessage;
 
 public class MessageProcessor {
 
@@ -25,7 +27,32 @@ public class MessageProcessor {
 			ServerMain.getInstance().startDatabasePoller();
 			return response;
 		}
+		if(m.getMessageType().equals(WriteMessage.class))
+		{
+			return processWriteMessage((WriteMessage) m);
+		}
 		return null;
+	}
+
+	/**
+	 * First put the entry in the server cache. 
+	 * Then send a synchronous request to Master Server to insert into the 
+	 * replicated DB.
+	 * If successful, remove from cache. 
+	 * @param m
+	 * @return 
+	 */
+	private MessageResponse processWriteMessage(WriteMessage m) {
+		PPSCache cache = ServerMain.getInstance().getCache();
+		cache.getPps().put(m.getKey(), m.getValue());
+		MessageResponse response = ServerMain.getInstance()
+				.getMessageSender().sendMessageToMasterServer(m);
+		if(response.isSuccess())
+		{
+			cache.getPps().remove(m.getKey());
+		}
+		//TODO: handle failures (need some kind of retry)
+		return response;
 	}
 	
 }
